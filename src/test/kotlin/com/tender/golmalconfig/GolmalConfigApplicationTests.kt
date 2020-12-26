bf53ee8e36904635e784a7ed089e80add3c11ecc
sushant patil <sushant.patil@lendico.de>
2020-12-26 22:44:12 +0100

    first commit

---
+++ src/test/kotlin/com/tender/golmalconfig/GolmalConfigApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tender.golmalconfig
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GolmalConfigApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
