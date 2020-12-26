986f1f4234718693293f58b020b6a30552f076da
sushant patil <sushant.patil@lendico.de>
2020-12-26 22:59:58 +0100

    Initial config

--- src/main/kotlin/com/tender/golmalconfig/GolmalConfigApplication.kt
+++ src/main/kotlin/com/tender/golmalconfig/GolmalConfigApplication.kt
@@ -2,8 +2,10 @@ package com.tender.golmalconfig
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class GolmalConfigApplication
 
 fun main(args: Array<String>) {
